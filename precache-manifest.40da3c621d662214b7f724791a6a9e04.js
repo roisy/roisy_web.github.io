57dd643dde7652b88e265e8f22db525fdd9faba1
Fernando M. Quintana <ferquinve@gmail.com>
2018-11-25 17:03:00 +0100

    Updates

---
+++ precache-manifest.40da3c621d662214b7f724791a6a9e04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d8835f9b2af7496f3f84",
+    "url": "/static/js/main.d8835f9b.chunk.js"
+  },
+  {
+    "revision": "79e251e83ae9a3d536d7",
+    "url": "/static/js/1.79e251e8.chunk.js"
+  },
+  {
+    "revision": "d8835f9b2af7496f3f84",
+    "url": "/static/css/main.e257434a.chunk.css"
+  },
+  {
+    "revision": "7d46678d2007fe9bbdbb5194e77ff225",
+    "url": "/index.html"
+  }
+];
